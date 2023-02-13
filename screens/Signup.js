e9dd994af57c9929f02ad420b556bd5744a6808c
Judy Zhou <dzhou224@uwo.ca>
2023-02-13 02:38:09 -0500

    initalized sign up!

--- screens/Signup.js
+++ screens/Signup.js
@@ -1,10 +1,97 @@
-import { Text } from 'react-native'
+import React, { useState } from 'react'
+
+import { Image, View, Text, Pressable, StyleSheet } from 'react-native';
+import { TextInput } from "@react-native-material/core";
+
+// let system = new System();
+
+function Login({ navigation }) {
+
+    const [name, setName] = useState('');
+
+    const [username, setUsername] = useState('');
+
+    const [email, setEmail] = useState('');
+
+    const [password, setPassword] = useState('');
 
 
-const Signup = () => {
     return (
-        <Text>Sign Up</Text>
-    )
+        <View id="signup" style={[styles.container]}>
+
+            <View>
+                <Text>Find your voice</Text>
+                <Text>Signup with your email</Text>
+                <View>
+                    <TextInput variant="outlined" style={styles.input} type="text"
+                        id='name' placeholder='Full Name'
+                        placeholderTextColor={'#000000'}
+                        onChangeText={newText => setName(newText)}
+                        defaultValue={name}>
+                    </TextInput>
+                    <TextInput variant="outlined" style={styles.input} type="text"
+                        id='email' placeholder='Email'
+                        placeholderTextColor={'#000000'}
+                        onChangeText={newText => setEmail(newText)}
+                        defaultValue={email}>
+                    </TextInput>
+                    <TextInput variant="outlined" style={styles.input} type="text"
+                        id='username' placeholder='Username'
+                        placeholderTextColor={'#000000'}
+                        onChangeText={newText => setUsername(newText)}
+                        defaultValue={username}>
+                    </TextInput>
+                    <TextInput variant="outlined" style={styles.input} type="password"
+                        id='password' placeholder='Password'
+                        placeholderTextColor={'#696969'}
+                        onChangeText={newText => setPassword(newText)}
+                        defaultValue={password}>
+                    </TextInput>
+                </View>
+                <Text>or</Text>
+
+                <View>
+
+                </View>
+                <Pressable id="execute" style={[styles.button]}
+                >
+                    <Text style={[styles.start]}>Login</Text>
+                </Pressable>
+
+                <Text>Already have an account? Login</Text>
+
+
+
+            </View>
+        </View>
+    );
+
 }
 
-export default Signup
+const styles = StyleSheet.create(
+    {
+        container: {
+            display: "flex",
+            flex: "1",
+            height: "100%",
+            width: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+
+        button: {
+
+        },
+
+        start: {
+
+        },
+
+        input: {
+            marginVertical: 5,
+        },
+
+    }
+)
+
+export default Login;
